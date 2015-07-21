c28d592f876eb1bfaf2b1f6fe65f7bf92281ab29
Colin Surprenant <colin.surprenant@gmail.com>
2015-07-21 16:17:18 -0400

    load and charset

--- src/main/java/com/jrubyconf2015/ByteBuffer.java
+++ src/main/java/com/jrubyconf2015/ByteBuffer.java
@@ -5,8 +5,12 @@ import java.io.File;
 import java.io.RandomAccessFile;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
+import java.nio.charset.StandardCharsets;
+import java.nio.charset.Charset;
 
 import org.jruby.RubyString;
+import org.jruby.util.ByteList;
+
 
 public class ByteBuffer {
   private final long size;
@@ -28,20 +32,32 @@ public class ByteBuffer {
     this.buffer.position(pos);
   }
 
-  public void put_bytes_copy(RubyString data) {
+  public void load()
+  {
+    this.buffer.load();
+  }
+
+  public void put_ruby_string_copy(RubyString data) {
     this.buffer.put(data.getByteList().bytes());
   }
 
-  public void put_bytes(RubyString data) {
-    this.buffer.put(data.getByteList().unsafeBytes());
+  public void put_ruby_string(RubyString data) {
+    ByteList byteList = data.getByteList();
+    this.buffer.put(byteList.unsafeBytes(), 0, byteList.length());
   }
 
   public void put_bytes(String data) {
-    this.buffer.put(data.getBytes());
+    byte[] bytes = data.getBytes();
+    this.buffer.put(bytes, 0, bytes.length);
+  }
+
+  public void put_bytes(String data, Charset charset) {
+    byte[] bytes = data.getBytes(charset);
+    this.buffer.put(bytes, 0, bytes.length);
   }
 
   public void put_bytes(byte[] data) {
-    this.buffer.put(data);
+    this.buffer.put(data, 0, data.length);
   }
 
   public void close()
