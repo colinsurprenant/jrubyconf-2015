7b78b4036b17e8d901ef7ef8422682f71860d6eb
Colin Surprenant <colin.surprenant@gmail.com>
2015-07-30 19:12:49 +0200

    write then read bench

---
+++ bench/queue_w_then_r.rb
@@ -0,0 +1,79 @@
+# encoding: utf-8
+
+$: << File.expand_path("../../", __FILE__)
+
+require "bundler/setup"
+require "benchmark"
+require "jruby-mmap-queues"
+require "lib/bench"
+
+# queue_rw.rb
+# test queue read/write using N/M consumers/producers
+# measure performance of completely writing and reading back all data
+
+Thread.abort_on_exception = true
+
+ITEMS = 2_000_000
+END_ITEM = "END"
+# PAGE_SIZE = ITEMS * JRubyConf2015::BUFFERS[1].bytesize
+PAGE_SIZE = JRubyConf2015::WRITE_SIZE
+# PAGE_SIZE = 512 * 1024 * 1024
+CACHE_SIZE = 2
+tps_results = []
+
+puts("=begin")
+
+[[1, 1], [1, 2], [2, 1], [2, 2]].each do |consumers_count, producers_count|
+# [[1, 3]].each do |consumers_count, producers_count|
+
+  definitions = [
+    {
+      :name => "Queue/PageCache",
+      :queue => Mmap::Queue.new(:page_handler => Mmap::PageCache.new(File.join(JRubyConf2015::OUT_PATH, "cached_mapped_queue_benchmark-#{consumers_count}-#{producers_count}"), :page_size => PAGE_SIZE, :cache_size => CACHE_SIZE))
+    },
+  ]
+
+  definitions.each do |definition|
+
+    b = Benchmark.bm(50) do |x|
+      x.report("consumers=#{consumers_count}, producers=#{producers_count}, #{definition[:name]}") do
+
+        queue = definition[:queue]
+        queue.clear
+
+
+        producers = producers_count.times.map do
+          Thread.new do
+            buffer = JRubyConf2015::BUFFERS[1]
+            ITEMS.times.each{|data| queue.push(buffer)}
+          end
+        end
+
+        producers.each(&:join)
+        consumers_count.times.each{queue.push(END_ITEM)}
+
+        consumers = consumers_count.times.map do
+          Thread.new do
+            while true
+              data = queue.pop
+              break if data == END_ITEM
+            end
+          end
+        end
+        consumers.each(&:join)
+
+        queue.purge
+      end
+    end
+
+    tps = (ITEMS * producers_count) / b.first.real
+    rate = (tps * JRubyConf2015::BUFFERS[1].bytesize) / (1024 * 1024)
+    tps_results << "consumers=#{consumers_count}, producers=#{producers_count}, #{definition[:name]} #{tps.to_i} TPS, #{rate.to_i}MB/s"
+  end
+end
+
+tps_results.each do |result|
+  puts(result)
+end
+
+puts("=end")
